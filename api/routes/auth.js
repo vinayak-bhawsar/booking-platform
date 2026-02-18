49ebcb9fa2bc76d6073cc3bd769c4b5b26cd17fe
Vinayak Bhawsar <admin@vinayak.com>
2026-02-18 22:44:53 +0530

    updates

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,5 +5,14 @@ const router = express.Router();
 
 router.post("/register", register)
 router.post("/login", login)
+router.post("/logout", (req, res) => {
+  res
+    .clearCookie("access_token", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("Logged out successfully.");
+});
 
 export default router
