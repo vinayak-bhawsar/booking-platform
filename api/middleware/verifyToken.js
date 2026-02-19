6cf0c312ebe6b8272b02f62ed5364d9862cba294
Vinayak Bhawsar <admin@vinayak.com>
2026-02-19 10:00:58 +0530

    Added verifyToken middleware

---
+++ api/middleware/verifyToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { createError } from "../utils/error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    return next(createError(401, "Not authenticated!"));
+  }
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "Token not valid!"));
+
+    req.user = user;
+    next();
+  });
+};
